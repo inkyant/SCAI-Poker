09fba0ceca00040d803644452a841402f7425c84
inkyant <80181074+inkyant@users.noreply.github.com>
2023-12-02 02:01:36 -0800

    add data for lin reg

--- linearRegression.py
+++ linearRegression.py
@@ -2,19 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+from query import query 
+
+# get data
+csvData = pd.read_csv("Poker_data.csv") 
+
+data_size = 1500
+
+X = csvData.iloc[list(range(data_size//2)) + list(range(-data_size//2, 0)), 0:14].to_numpy()
+y = csvData.iloc[list(range(data_size//2)) + list(range(-data_size//2, 0)), 14].to_numpy()
+
+# train-test split for model evaluation
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75, shuffle=True)
 
 
 reg = linear_model.LinearRegression()
-x = np.append(np.random.randint(0, 6, (500, 13)), np.random.randint(7, 13, (500, 13)), axis=0)
-y = np.append(np.zeros((500)), np.ones(500), axis=0)
-reg.fit(x, y)
+reg.fit(X_train, y_train)
 
-yPred = reg.predict(x)
+yPred = reg.predict(X_test)
 
 
 print("Coefficients: \n", reg.coef_)
-print("Mean squared error: %.2f" % mean_squared_error(y, yPred))
-print("Coefficient of determination: %.2f" % r2_score(y, yPred))
+print("Mean squared error: %.2f" % mean_squared_error(y_test, yPred))
+print("Coefficient of determination: %.2f" % r2_score(y_test, yPred))
 
 
 
